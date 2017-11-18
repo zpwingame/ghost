ca92e2afd92fa7c1470677220bb6904e875edb6b
zhaopeng <1175420363@qq.com>
2017-11-18 16:53:35 +0800

    remove dist foler

--- dist/index1.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],[function(n,o,c){n.exports=c(1)},function(n,o){}],[0]);
-//# sourceMappingURL=index1.js.map
--- dist/treeshaking.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1],{10:function(e,o,t){"use strict";function n(){return"foo"}o.a=n},8:function(e,o,t){e.exports=t(9)},9:function(e,o,t){"use strict";Object.defineProperty(o,"__esModule",{value:!0});var n=t(10);console.log(Object(n.a)())}},[8]);
-//# sourceMappingURL=treeshaking.js.map
